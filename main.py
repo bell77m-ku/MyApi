8b1281902482b0447d699c2c6df20f735dd36872
bell77m <bell77m@gmail.com>
2024-01-25 01:20:35 +0700

    edit /students/<int:id> add post_students

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify, request
 from flask_basicauth import BasicAuth
-from functools import wraps
+
 
 app = Flask(__name__)
 app.config['BASIC_AUTH_USERNAME']='username'
 app.config['BASIC_AUTH_PASSWORD']='12345678'
+basic_auth=BasicAuth(app)
 
-students = [{"id":0, "name": "bunyarit", "std_id":"6530300317", "major": "T12"}]
+students = [{"name": "bunyarit", "std_id":6530300317, "major": "T12"}]
+
 
 @app.route('/')
 def welcome():
@@ -14,24 +16,38 @@ def welcome():
 
 
 @app.route('/students', methods=["GET"])
+@basic_auth.required
 def get_students():
     return jsonify({"students": students})
 
-@app.route('/students/<int:id>', methods=["GET"])
-def get_students(id):
-    student = next((s for s in students if s["id"]==id), None)
+@app.route('/students/<int:std_id>', methods=["GET"])
+@basic_auth.required
+def get_student(std_id):
+    student = next((s for s in students if s["std_id"]==std_id), None)
     if student:
-        return jsonify({"students": students})
+        return jsonify({"students": students}), 200
     else:
-        return jsonify({"error":"Student not found"})
+        return jsonify({"error":"Student not found"}), 404
 
 
 @app.route('/students', methods=["POST"])
+@basic_auth.required
 def post_students():
-    return jsonify({"students": students})
+    data = request.get_json()
+    std_id = next((s for s in students if s["std_id"] != data["std_id"]), None)
+    if std_id:
+        new_std = {
+            "name": data["name"],
+            "std_id": data["std_id"],
+            "major": data["major"]
+        }
+        students.append(new_std)
+        return jsonify(new_std), 200
+    else:
+        return jsonify({"error":"Cannot create new student"})
 
-def run(host="localhost", port=80):
+def run(host="127.0.0.1", port=80):
     app.run(host=host, port=port, debug=True)
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     run()
