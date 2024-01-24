143c358cfac611377672fefae15a308af9d1630b
bell77m <bell77m@gmail.com>
2024-01-25 00:51:25 +0700

    add /students/<int:id>

--- main.py
+++ main.py
@@ -6,16 +6,29 @@ app = Flask(__name__)
 app.config['BASIC_AUTH_USERNAME']='username'
 app.config['BASIC_AUTH_PASSWORD']='12345678'
 
-students = [{"name": "bunyarit", "id":"6530300317", "major": "T12"}]
+students = [{"id":0, "name": "bunyarit", "std_id":"6530300317", "major": "T12"}]
 
 @app.route('/')
 def welcome():
     return "<h1>Welcome to Student Management API</h1>"
 
+
 @app.route('/students', methods=["GET"])
 def get_students():
     return jsonify({"students": students})
 
+@app.route('/students/<int:id>', methods=["GET"])
+def get_students(id):
+    student = next((s for s in students if s["id"]==id), None)
+    if student:
+        return jsonify({"students": students})
+    else:
+        return jsonify({"error":"Student not found"})
+
+
+@app.route('/students', methods=["POST"])
+def post_students():
+    return jsonify({"students": students})
 
 def run(host="localhost", port=80):
     app.run(host=host, port=port, debug=True)
